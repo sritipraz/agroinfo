375c8db87144d907b94d74fadf936b217efb431f
sritipraz <prajapatisriti.sp@gmail.comhttps://github.com/sritipraz/class3.git>
2017-06-05 23:00:51 +0545

    Add project files.

---
+++ Agroin4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Agroin4.MvcApplication" Language="C#" %>
---
+++ Agroin4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Agroin4.Startup))]
+namespace Agroin4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
