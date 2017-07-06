b753368d129b66cc8b821bc66b3e4b497e93bb1e
sritipraz <prajapatisriti.sp@gmail.comhttps://github.com/sritipraz/class3.git>
2017-07-06 16:08:15 +0545

    try again

--- Agroin4/Controllers/articleController.cs
+++ Agroin4/Controllers/articleController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Agroin4.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,11 +8,30 @@ using System.Web.Mvc;
 namespace Agroin4.Controllers
 {
     public class articleController : Controller
-    {
+    { private webAppModel db = new webAppModel();
+    
         // GET: article
-        public ActionResult Index()
+        public ActionResult Create()
+
         {
+            ViewData["IdcropList"] = db.crops.Select(p => new SelectListItem() { Text = p.crop_name, Value = p.id.ToString() }).AsEnumerable();
+
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create( article articleobj)
+        {
+            if (ModelState.IsValid)
+            {
+                db.articles.Add(articleobj);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(articleobj);
+        }
+
     }
 }
